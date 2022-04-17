50145db59b8f0f222b18e3335ce14bb1225be6d9
amineayadi <aayadi@five-consulting.com>
2022-04-17 23:04:21 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { SidemenuComponent } from './components/layout/sidemenu/sidemenu.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+    HeaderComponent,
+    SidemenuComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
